fc3fa73e0dcdfdbf43fc8eb1a5bdf5fa8b23e27a
ErvinCs <csoka.ervin12@gmail.com>
2019-03-20 23:22:42 +0200

    Mostly working; TODO - Fix DataSources

--- DBMS-Lab01/DBMS-Lab01/Form1.Designer.cs
+++ DBMS-Lab01/DBMS-Lab01/Form1.Designer.cs
@@ -29,13 +29,11 @@
         private void InitializeComponent()
         {
             this.btnAddDoctor = new System.Windows.Forms.Button();
-            this.viewDoctors = new System.Windows.Forms.ListView();
             this.labelDoctors = new System.Windows.Forms.Label();
-            this.viewRequests = new System.Windows.Forms.ListView();
             this.labelRequests = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelDateOfBirth = new System.Windows.Forms.Label();
+            this.labelCNP = new System.Windows.Forms.Label();
             this.labelQuantity = new System.Windows.Forms.Label();
             this.labelRH = new System.Windows.Forms.Label();
             this.labelGroup = new System.Windows.Forms.Label();
@@ -44,12 +42,17 @@
             this.tbBirthDate = new System.Windows.Forms.TextBox();
             this.tbCNP = new System.Windows.Forms.TextBox();
             this.tbQuantity = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.tbBloodGroup = new System.Windows.Forms.TextBox();
+            this.tbRH = new System.Windows.Forms.TextBox();
+            this.tbDoctorID = new System.Windows.Forms.TextBox();
             this.btnAddRequest = new System.Windows.Forms.Button();
             this.btnDeleteRequest = new System.Windows.Forms.Button();
             this.btnUpdateRequest = new System.Windows.Forms.Button();
+            this.viewDoctors = new System.Windows.Forms.DataGridView();
+            this.viewRequests = new System.Windows.Forms.DataGridView();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.viewDoctors)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viewRequests)).BeginInit();
             this.SuspendLayout();
             // 
             // btnAddDoctor
@@ -61,15 +64,7 @@
             this.btnAddDoctor.TabIndex = 0;
             this.btnAddDoctor.Text = "Add Doctor";
             this.btnAddDoctor.UseVisualStyleBackColor = true;
-            // 
-            // viewDoctors
-            // 
-            this.viewDoctors.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.viewDoctors.Location = new System.Drawing.Point(17, 37);
-            this.viewDoctors.Name = "viewDoctors";
-            this.viewDoctors.Size = new System.Drawing.Size(423, 270);
-            this.viewDoctors.TabIndex = 1;
-            this.viewDoctors.UseCompatibleStateImageBehavior = false;
+            this.btnAddDoctor.Click += new System.EventHandler(this.btnAddDoctor_Click);
             // 
             // labelDoctors
             // 
@@ -80,16 +75,6 @@
             this.labelDoctors.Size = new System.Drawing.Size(100, 25);
             this.labelDoctors.TabIndex = 2;
             this.labelDoctors.Text = "Doctors:";
-            this.labelDoctors.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // viewRequests
-            // 
-            this.viewRequests.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.viewRequests.Location = new System.Drawing.Point(489, 37);
-            this.viewRequests.Name = "viewRequests";
-            this.viewRequests.Size = new System.Drawing.Size(423, 270);
-            this.viewRequests.TabIndex = 3;
-            this.viewRequests.UseCompatibleStateImageBehavior = false;
             // 
             // labelRequests
             // 
@@ -101,36 +86,35 @@
             this.labelRequests.TabIndex = 4;
             this.labelRequests.Text = "Requests:";
             // 
-            // label3
+            // labelName
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(13, 340);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(55, 20);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Name:";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.Location = new System.Drawing.Point(13, 340);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(55, 20);
+            this.labelName.TabIndex = 5;
+            this.labelName.Text = "Name:";
             // 
-            // label4
+            // labelDateOfBirth
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(13, 375);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(101, 20);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Date of birth:";
+            this.labelDateOfBirth.AutoSize = true;
+            this.labelDateOfBirth.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDateOfBirth.Location = new System.Drawing.Point(13, 408);
+            this.labelDateOfBirth.Name = "labelDateOfBirth";
+            this.labelDateOfBirth.Size = new System.Drawing.Size(101, 20);
+            this.labelDateOfBirth.TabIndex = 6;
+            this.labelDateOfBirth.Text = "Date of birth:";
             // 
-            // label5
+            // labelCNP
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(13, 411);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(45, 20);
-            this.label5.TabIndex = 7;
-            this.label5.Text = "CNP:";
+            this.labelCNP.AutoSize = true;
+            this.labelCNP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCNP.Location = new System.Drawing.Point(13, 375);
+            this.labelCNP.Name = "labelCNP";
+            this.labelCNP.Size = new System.Drawing.Size(45, 20);
+            this.labelCNP.TabIndex = 7;
+            this.labelCNP.Text = "CNP:";
             // 
             // labelQuantity
             // 
@@ -141,7 +125,6 @@
             this.labelQuantity.Size = new System.Drawing.Size(72, 20);
             this.labelQuantity.TabIndex = 8;
             this.labelQuantity.Text = "Quantity:";
-            this.labelQuantity.Click += new System.EventHandler(this.label6_Click);
             // 
             // labelRH
             // 
@@ -184,7 +167,7 @@
             // tbBirthDate
             // 
             this.tbBirthDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbBirthDate.Location = new System.Drawing.Point(129, 372);
+            this.tbBirthDate.Location = new System.Drawing.Point(129, 401);
             this.tbBirthDate.Name = "tbBirthDate";
             this.tbBirthDate.Size = new System.Drawing.Size(100, 26);
             this.tbBirthDate.TabIndex = 14;
@@ -192,7 +175,7 @@
             // tbCNP
             // 
             this.tbCNP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbCNP.Location = new System.Drawing.Point(129, 408);
+            this.tbCNP.Location = new System.Drawing.Point(129, 369);
             this.tbCNP.Name = "tbCNP";
             this.tbCNP.Size = new System.Drawing.Size(100, 26);
             this.tbCNP.TabIndex = 15;
@@ -205,29 +188,29 @@
             this.tbQuantity.Size = new System.Drawing.Size(100, 26);
             this.tbQuantity.TabIndex = 16;
             // 
-            // textBox5
+            // tbBloodGroup
             // 
-            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox5.Location = new System.Drawing.Point(610, 405);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 26);
-            this.textBox5.TabIndex = 17;
+            this.tbBloodGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbBloodGroup.Location = new System.Drawing.Point(610, 405);
+            this.tbBloodGroup.Name = "tbBloodGroup";
+            this.tbBloodGroup.Size = new System.Drawing.Size(100, 26);
+            this.tbBloodGroup.TabIndex = 17;
             // 
-            // textBox6
+            // tbRH
             // 
-            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox6.Location = new System.Drawing.Point(610, 369);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 26);
-            this.textBox6.TabIndex = 18;
+            this.tbRH.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbRH.Location = new System.Drawing.Point(610, 369);
+            this.tbRH.Name = "tbRH";
+            this.tbRH.Size = new System.Drawing.Size(100, 26);
+            this.tbRH.TabIndex = 18;
             // 
-            // textBox7
+            // tbDoctorID
             // 
-            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox7.Location = new System.Drawing.Point(610, 437);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(100, 26);
-            this.textBox7.TabIndex = 19;
+            this.tbDoctorID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbDoctorID.Location = new System.Drawing.Point(610, 437);
+            this.tbDoctorID.Name = "tbDoctorID";
+            this.tbDoctorID.Size = new System.Drawing.Size(100, 26);
+            this.tbDoctorID.TabIndex = 19;
             // 
             // btnAddRequest
             // 
@@ -238,6 +221,7 @@
             this.btnAddRequest.TabIndex = 20;
             this.btnAddRequest.Text = "Add Request";
             this.btnAddRequest.UseVisualStyleBackColor = true;
+            this.btnAddRequest.Click += new System.EventHandler(this.btnAddRequest_Click);
             // 
             // btnDeleteRequest
             // 
@@ -248,6 +232,7 @@
             this.btnDeleteRequest.TabIndex = 21;
             this.btnDeleteRequest.Text = "Delete Request";
             this.btnDeleteRequest.UseVisualStyleBackColor = true;
+            this.btnDeleteRequest.Click += new System.EventHandler(this.btnDeleteRequest_Click);
             // 
             // btnUpdateRequest
             // 
@@ -258,18 +243,47 @@
             this.btnUpdateRequest.TabIndex = 22;
             this.btnUpdateRequest.Text = "Update Request";
             this.btnUpdateRequest.UseVisualStyleBackColor = true;
+            this.btnUpdateRequest.Click += new System.EventHandler(this.btnUpdateRequest_Click);
+            // 
+            // viewDoctors
+            // 
+            this.viewDoctors.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.viewDoctors.Location = new System.Drawing.Point(17, 37);
+            this.viewDoctors.Name = "viewDoctors";
+            this.viewDoctors.Size = new System.Drawing.Size(423, 270);
+            this.viewDoctors.TabIndex = 23;
+            this.viewDoctors.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.viewDoctors_CellClick);
+            // 
+            // viewRequests
+            // 
+            this.viewRequests.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.viewRequests.Location = new System.Drawing.Point(489, 37);
+            this.viewRequests.Name = "viewRequests";
+            this.viewRequests.Size = new System.Drawing.Size(423, 270);
+            this.viewRequests.TabIndex = 24;
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker.Location = new System.Drawing.Point(129, 433);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(311, 22);
+            this.dateTimePicker.TabIndex = 25;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(924, 545);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.viewRequests);
+            this.Controls.Add(this.viewDoctors);
             this.Controls.Add(this.btnUpdateRequest);
             this.Controls.Add(this.btnDeleteRequest);
             this.Controls.Add(this.btnAddRequest);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.tbDoctorID);
+            this.Controls.Add(this.tbRH);
+            this.Controls.Add(this.tbBloodGroup);
             this.Controls.Add(this.tbQuantity);
             this.Controls.Add(this.tbCNP);
             this.Controls.Add(this.tbBirthDate);
@@ -278,17 +292,17 @@
             this.Controls.Add(this.labelGroup);
             this.Controls.Add(this.labelRH);
             this.Controls.Add(this.labelQuantity);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelCNP);
+            this.Controls.Add(this.labelDateOfBirth);
+            this.Controls.Add(this.labelName);
             this.Controls.Add(this.labelRequests);
-            this.Controls.Add(this.viewRequests);
             this.Controls.Add(this.labelDoctors);
-            this.Controls.Add(this.viewDoctors);
             this.Controls.Add(this.btnAddDoctor);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.viewDoctors)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viewRequests)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -297,13 +311,11 @@
         #endregion
 
         private System.Windows.Forms.Button btnAddDoctor;
-        private System.Windows.Forms.ListView viewDoctors;
         private System.Windows.Forms.Label labelDoctors;
-        private System.Windows.Forms.ListView viewRequests;
         private System.Windows.Forms.Label labelRequests;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelDateOfBirth;
+        private System.Windows.Forms.Label labelCNP;
         private System.Windows.Forms.Label labelQuantity;
         private System.Windows.Forms.Label labelRH;
         private System.Windows.Forms.Label labelGroup;
@@ -312,12 +324,15 @@
         private System.Windows.Forms.TextBox tbBirthDate;
         private System.Windows.Forms.TextBox tbCNP;
         private System.Windows.Forms.TextBox tbQuantity;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox tbBloodGroup;
+        private System.Windows.Forms.TextBox tbRH;
+        private System.Windows.Forms.TextBox tbDoctorID;
         private System.Windows.Forms.Button btnAddRequest;
         private System.Windows.Forms.Button btnDeleteRequest;
         private System.Windows.Forms.Button btnUpdateRequest;
+        private System.Windows.Forms.DataGridView viewDoctors;
+        private System.Windows.Forms.DataGridView viewRequests;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
     }
 }
 
--- DBMS-Lab01/DBMS-Lab01/Form1.cs
+++ DBMS-Lab01/DBMS-Lab01/Form1.cs
@@ -6,12 +6,20 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace DBMS_Lab01
 {
     public partial class Form1 : Form
     {
+        private static String connectionString = @"Data Source=DESKTOP-AFL01LP;Initial Catalog=Lab01;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+        //TODO - Binding Source, DataSet and this (right below) adapter
+        SqlDataAdapter adapter;
+        BindingSource bs = new BindingSource();
+        DataSet ds = new DataSet();
+
         public Form1()
         {
             InitializeComponent();
@@ -19,22 +27,207 @@ namespace DBMS_Lab01
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            try
+            {
+                connection.Open();
+                MessageBox.Show(connection.State.ToString());
 
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Error: " + err.Message.ToString());
+            }
+            updateViewDoctors();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void updateViewRequests(int id)
         {
+            viewRequests.DataSource = null;
 
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * FROM Requests WHERE requests_doctor = @request_id";
+            cmd.Parameters.AddWithValue("@request_id", id);
+
+            DataSet dts = new DataSet();
+            SqlDataAdapter ad = new SqlDataAdapter(cmd);
+            ad.Fill(dts, "Requests");
+            viewRequests.DataSource = dts.Tables["Requests"];
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        private void updateViewDoctors()
         {
+            adapter = new SqlDataAdapter("SELECT * FROM Doctors", connection);
+            adapter.Fill(ds, "Doctors");
+            bs.DataSource = ds.Tables["Doctors"];
+            viewDoctors.DataSource = bs;
 
+            int id;
+            int.TryParse(viewDoctors.CurrentRow.Cells[0].Value.ToString(), out id);
+            updateViewRequests(id);
         }
 
-        private void label6_Click(object sender, EventArgs e)
+        private void viewDoctors_CellClick(object sender, DataGridViewCellEventArgs e)
         {
+            int doctorId;
+            int.TryParse(viewDoctors.CurrentRow.Cells[0].Value.ToString(), out doctorId);
 
+            updateViewRequests(doctorId);
+        }
+
+        private void btnAddDoctor_Click(object sender, EventArgs e)
+        {
+            int doctorId;
+            int.TryParse(this.viewDoctors.CurrentRow.Cells[0].Value.ToString(), out doctorId);
+
+            String name = tbName.Text;
+
+            DateTime date;
+            date = dateTimePicker.Value.Date;
+            String birthDate = date.Day.ToString() + "/" + date.Month.ToString() + "/" + date.Year.ToString();
+
+            //DateTime birthDate;
+            //DateTime.TryParse(this.tbBirthDate.Text, out birthDate);
+            //birthDate = birthDate.Date;
+
+            long CNP;
+            long.TryParse(this.tbCNP.Text, out CNP);
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "INSERT into Doctors VALUES (@doctor_id, @name, @birth_date, @CNP)";
+                cmd.Parameters.AddWithValue("@doctor_id", doctorId);
+                cmd.Parameters.AddWithValue("@name", name);
+                cmd.Parameters.AddWithValue("@birth_date", birthDate);
+                cmd.Parameters.AddWithValue("@CNP", CNP);
+                cmd.ExecuteNonQuery();
+
+                updateViewRequests(doctorId);
+            }
+            catch (SqlException err)
+            {
+                MessageBox.Show("SQLException: " + err.ToString());
+            } catch (Exception err)
+            {
+                MessageBox.Show("Exception: " + err.ToString());
+            }
+
+            tbName.Clear();
+            tbBirthDate.Clear();
+            tbCNP.Clear();
+        }
+
+        private void btnAddRequest_Click(object sender, EventArgs e)
+        {
+            int quantity;
+            int.TryParse(tbQuantity.Text, out quantity);
+
+            String rh = tbRH.Text;
+
+            String bloodGroup = tbBloodGroup.Text;
+
+            int doctorId;
+            int.TryParse(tbDoctorID.Text, out doctorId);
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "INSERT into Requests VALUES (@quantity, @rh, @blood_group, @requests_doctor)";
+                cmd.Parameters.AddWithValue("@quantity", quantity);
+                cmd.Parameters.AddWithValue("@rh", rh);
+                cmd.Parameters.AddWithValue("@blood_group", bloodGroup);
+                cmd.Parameters.AddWithValue("@requests_doctor", doctorId);
+                cmd.ExecuteNonQuery();
+
+                updateViewRequests(doctorId);
+            }
+            catch (SqlException err)
+            {
+                MessageBox.Show(err.ToString());
+            } catch (Exception err)
+            {
+                MessageBox.Show("SQLError: " + err.ToString());
+            }
+
+            tbQuantity.Clear();
+            tbRH.Clear();
+            tbBloodGroup.Clear();
+            tbDoctorID.Clear();
+        }
+
+        private void btnDeleteRequest_Click(object sender, EventArgs e)
+        {
+            int doctorId;
+            int.TryParse(viewDoctors.CurrentRow.Cells[0].Value.ToString(), out doctorId);
+
+            try
+            {
+                int requestId;
+                int.TryParse(viewRequests.CurrentRow.Cells[0].Value.ToString(), out requestId);
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "DELETE FROM Requests WHERE request_id = @request_id";
+                cmd.Parameters.AddWithValue("@request_id", requestId);
+                cmd.ExecuteNonQuery();
+
+                updateViewRequests(doctorId);
+            }
+            catch (SqlException err)
+            {
+                MessageBox.Show(err.ToString());
+            } catch (Exception err)
+            {
+                MessageBox.Show("SQLError: " + err.ToString());
+            }
+
+            tbQuantity.Clear();
+            tbRH.Clear();
+            tbBloodGroup.Clear();
+            tbDoctorID.Clear();
+        }
+
+        private void btnUpdateRequest_Click(object sender, EventArgs e)
+        {
+            int requestId;
+            int.TryParse(viewRequests.CurrentRow.Cells[0].Value.ToString(), out requestId);
+
+            int quantity;
+            int.TryParse(tbQuantity.Text, out quantity);
+
+            String rh = tbRH.Text;
+
+            String bloodGroup = tbBloodGroup.Text;
+
+            int doctorId;
+            int.TryParse(tbDoctorID.Text, out doctorId);
+
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "UPDATE Requests SET requests_doctor = @requests_doctor, quantity = @quantity, rh = @rh, blood_group = @blood_group " +
+                                "WHERE request_id = @request_id";
+            cmd.Parameters.AddWithValue("@request_id", requestId);
+            cmd.Parameters.AddWithValue("@quantity", quantity);
+            cmd.Parameters.AddWithValue("@rh", rh);
+            cmd.Parameters.AddWithValue("@blood_group", bloodGroup);
+            cmd.Parameters.AddWithValue("@requests_doctor", doctorId);
+            cmd.ExecuteNonQuery();
+
+            updateViewRequests(doctorId);
+
+            tbQuantity.Clear();
+            tbRH.Clear();
+            tbBloodGroup.Clear();
+            tbDoctorID.Clear();
         }
     }
 }
